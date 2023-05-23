410f4d5adaa03f14fdc1210844d2cc1e26e3db91
samshervin <samshervin23@gmail.com>
2023-05-23 15:54:56 +0530

    app init

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <div>
+        <section className='text-blue-800 hover:scale-150'>
+          Ash
+        </section>
+        <a
+          className=""
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a></div>
+  );
+}
+
+export default App;
