5a8dffea089d44e8a496018ad4bee3badcc0ea4d
samshervin <samshervin23@gmail.com>
2023-05-23 19:31:35 +0530

    Tailwind problem solved and now is working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,13 @@
+import React from "react";
+import "./css/index.css"
 
 function App() {
   return (
     <div>
-        <section className='text-blue-800 hover:scale-150'>
-          Ash
-        </section>
-        <a
-          className=""
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a></div>
+    <section className="hover:scale-150 text-center text-blue-500 font-extrabold text-8xl">
+      Hello world!
+    </section>
+    </div>
   );
 }
 
